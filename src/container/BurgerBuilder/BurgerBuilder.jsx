994d619fe6f1d49e0a60267ad757f24d4a15ea28
joejoh <georgeja.18@student.funaab.edu.ng>
2025-08-21 12:22:45 +0100

    mor button

--- src/container/BurgerBuilder/BurgerBuilder.jsx
+++ src/container/BurgerBuilder/BurgerBuilder.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import Burger from '../../components/Burger/BurgerIngredient/Burger'
 import BuildControls from '../../components/Burger/BuildsControls/BuildControls'
 
+
+
+const INGREDIENT_PRICES = {
+    salad : 0.5,
+    cheese : 0.4,
+    meat : 1.3,
+    bacon : 0.7
+}
 class BurgerBuilder extends Component{
     state = {
         ingredient :{
@@ -10,14 +18,34 @@ class BurgerBuilder extends Component{
             cheese : 0,
             meat : 0
 
-        }
+        }, 
+        totalPrice : 4
+    }
+
+    addIngredientHandler = (type) => {
+       const oldCount  = this.state.ingredient[type];
+       const updatedCount = oldCount + 1;
+       const updatedIngredients = {
+          ...this.state.ingredient
+       };
+       updatedIngredients[type] = updatedCount
+       const  priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice : newPrice, ingredient : updatedIngredients});
+
+    } 
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render(){
         return (
             <>
              <Burger ingredients={this.state.ingredient} /> 
-            <BuildControls/>
+            <BuildControls
+              ingredientAdded = {this.addIngredientHandler} />
             </>
         )
     }
