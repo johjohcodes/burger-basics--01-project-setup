d170c4c0e483d6ffc9ee3f3f85e430654de104f3
joejoh <georgeja.18@student.funaab.edu.ng>
2025-09-08 22:37:38 +0100

    all i can say is omo

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -4,11 +4,13 @@ import './Toolbar.css'
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../Navigationitems/NavigationItems";
 
+
 const Toolbar = (props) => (
     <header className="Toolbar">
         <div className="Menu">MENU</div>
         <div className="Logo">
-           <Logo />
+            {/* Replace 'burger.png' with your actual image file name and path */}
+            <img src="/assets/images/burger.png" alt="Burger" style={{height: '100%', width: 'auto'}} />
         </div>
         <nav className="DesktopOnly">
             <NavigationItems/>
