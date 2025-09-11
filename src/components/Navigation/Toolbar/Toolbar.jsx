41745c818349f271d839a3fdd1cb433e6c85b4a2
joejoh <georgeja.18@student.funaab.edu.ng>
2025-09-11 22:35:57 +0100

    bugs on my neck

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 
-import './Toolbar.css'
-import Logo from "../../Logo/Logo";
+import './Toolbar.css';
 import NavigationItems from "../Navigationitems/NavigationItems";
-
+// import burgerLogo from '../../assets/images/burgerLogo.png';
 
 const Toolbar = (props) => (
     <header className="Toolbar">
-        <div className="Menu">MENU</div>
+        <div className="Menu" onClick={props.openSideDrawer}>MENU</div>
         <div className="Logo">
-            {/* Replace 'burger.png' with your actual image file name and path */}
-            <img src="/assets/images/burger.png" alt="Burger" style={{height: '100%', width: 'auto'}} />
+            <img src="/assets/images/burgerLogo.png" alt="Burger" style={{height: '100%', width: 'auto'}} />
         </div>
         <nav className="DesktopOnly">
             <NavigationItems/>
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,19 +4,29 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
-    render(){
-         return (
-        <>
-            
-            <Toolbar/>
-            <SideDrawer/>
-            <main className= 'Content'>
-                {props.children}
-            </main>
-        </>
-    );
+    state = {
+        showSideDrawer: false
     }
-   
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerOpenHandler = () => {
+        this.setState({ showSideDrawer: true });
+    }
+
+    render() {
+        return (
+            <>
+                <Toolbar openSideDrawer={this.sideDrawerOpenHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className='Content'>
+                    {this.props.children}
+                </main>
+            </>
+        );
+    }
 }
 
 export default Layout;
