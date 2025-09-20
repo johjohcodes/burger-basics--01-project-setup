5d6ac675992f16cc707847f6ba0a0a90d36a2c52
joejoh <georgeja.18@student.funaab.edu.ng>
2025-09-20 23:25:07 +0100

    welloh well

--- src/components/UI/Backdrop/Backdrop.jsx
+++ src/components/UI/Backdrop/Backdrop.jsx
@@ -3,13 +3,25 @@ import React from "react";
 import './Backdrop.css'
 
 
-const Backdrop = (props) =>(
+const Backdrop = (props) => {
+    const handleClick = () => {
+        console.log('Backdrop clicked!'); // Debug log
+        if (props.clicked) {
+            props.clicked();
+        } else {
+            console.log('No clicked handler provided!'); // Debug log
+        }
+    };
 
- props.show ? <div className="Backdrop"  onClick={props.clicked}></div> : null
+    console.log('Backdrop props:', props); // Debug log
 
-   
-)
-
-
-
+    return props.show ? (
+        <div className="Backdrop" onClick={handleClick}>
+            {/* Temporary debug text - remove after fixing */}
+            <div style={{color: 'white', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+                Click to close
+            </div>
+        </div>
+    ) : null;
+};
 export default Backdrop;
