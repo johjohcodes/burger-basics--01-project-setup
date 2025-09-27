7bba073ee0617579ec9fb670924c14157a93e35d
joejoh <georgeja.18@student.funaab.edu.ng>
2025-09-27 23:19:45 +0100

    dayy

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import './DrawerToggle.css';
 
-
-const DrawerToggle = (props) => {
-    <div onClick={props.clicked} >
+const DrawerToggle = (props) => (
+    <div className="DrawerToggle" onClick={props.clicked}>
         <div></div>
         <div></div>
         <div></div>
     </div>
-}
+);
 
 export default DrawerToggle;
