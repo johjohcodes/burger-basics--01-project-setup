ff1a1270ff30ddd8eb4410efc9e2bfafb55cd6fa
joejoh <georgeja.18@student.funaab.edu.ng>
2025-10-08 22:04:54 +0100

    twoo

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../UI/Button/Button";
 
-const OrderSummary = (props) => {
-   const ingredientSummary = Object.keys(props.ingredient)
+class OrderSummary extends Component {
+
+  render(){
+    const ingredientSummary = Object.keys(props.ingredient)
          .map(igKey => (
            <li key={igKey }><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredient[igKey]}</li>
          ));
-    
     return (
-        <>
+           <>
           <h3>Your Order</h3>
           <p> A delicious burger with the following ingredient:</p>
           <ul>
             {ingredientSummary}
 
           </ul>
-          <p><strong>Total price : {props.price.toFixed(2)}</strong></p>
+          <p><strong>Total price : {this.props.price.toFixed(2)}</strong></p>
           <p>CONTINUE TO CHECK OUT?</p>
           <Button btnType = "Danger" clicked = {props.purchaseCancelled}>CANCEL </Button>
           <Button btnType = "Success" clicked = {props.purchaseContinued}>CONTINUE</Button>
         </>
     )
-}
+  }
+} 
+   
+    
+    
 
 export default OrderSummary;
