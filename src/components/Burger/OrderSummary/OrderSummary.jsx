59738299f91c698658619df5e6b202f5ae1624a0
joejoh <georgeja.18@student.funaab.edu.ng>
2025-10-29 21:53:53 +0100

    ahhhh

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -4,28 +4,28 @@ import Button from "../../UI/Button/Button";
 class OrderSummary extends Component {
 
   render(){
-    const ingredientSummary = Object.keys(props.ingredient)
+    const ingredients = this.props.ingredients || this.props.ingredient || {};
+    const ingredientSummary = Object.keys(ingredients)
          .map(igKey => (
-           <li key={igKey }><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredient[igKey]}</li>
+           <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {ingredients[igKey]}</li>
          ));
-    return (
-           <>
-          <h3>Your Order</h3>
-          <p> A delicious burger with the following ingredient:</p>
-          <ul>
-            {ingredientSummary}
 
-          </ul>
-          <p><strong>Total price : {this.props.price.toFixed(2)}</strong></p>
-          <p>CONTINUE TO CHECK OUT?</p>
-          <Button btnType = "Danger" clicked = {props.purchaseCancelled}>CANCEL </Button>
-          <Button btnType = "Success" clicked = {props.purchaseContinued}>CONTINUE</Button>
-        </>
+    const price = Number(this.props.price || 0);
+
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total price: {price.toFixed(2)}</strong></p>
+        <p>CONTINUE TO CHECK OUT?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </>
     )
   }
-} 
-   
-    
-    
+}
 
 export default OrderSummary;
